095fa64c8b394c42ba0f1c09bf030112c4d73c89
toPayton <60796560+toPayton@users.noreply.github.com>
2023-12-02 18:54:19 -0500

    remove problem

---
+++ traj_cleaning/convert_html.py
@@ -0,0 +1,82 @@
+import argparse
+from browser_env.envs import ScriptBrowserEnv
+import json
+import os
+import re
+import random
+
+
+path_of_this_file = os.path.dirname(os.path.realpath(__file__))
+
+
+env = ScriptBrowserEnv(
+    headless=True,
+    slow_mo=0,
+    observation_type="accessibility_tree",
+    current_viewport_only=False,
+    viewport_size={
+        "width": 1280,
+        "height": 720,
+    },
+    save_trace_enabled=False,
+    sleep_after_execution=0.0,
+)
+
+
+def convert(url, save_path_no_ext=None):
+    random_id = random.randint(0, 10000000000)
+    with open(f"{path_of_this_file}/tmp_config_{random_id}.json", "w") as f:
+        f.write(
+            f"""
+                {{
+                    "storage_state": null,
+                    "start_url": "{url}",
+                    "geolocation": null
+                }}
+            """
+        )
+
+    obs, info = env.reset(options={"config_file": f"{path_of_this_file}/tmp_config_{random_id}.json"})
+
+    # remove tmp_config_{random_id}.json
+    os.remove(f"{path_of_this_file}/tmp_config_{random_id}.json")
+
+    pattern = re.compile(r"[a-zA-Z0-9]")
+
+    new_tree = []
+
+    for line in obs["text"].split("\n"):
+        content = " ".join(line.split("'")[1:])
+        if bool(pattern.search(content)):
+            new_tree.append(line)
+
+    if not save_path_no_ext is None:
+        with open(f"{save_path_no_ext}.tree", "w") as f:
+            f.write("\n".join(new_tree))
+
+        with open(f"{save_path_no_ext}.json", "w") as f:
+            json.dump(info["observation_metadata"], f, indent=4)
+
+    return new_tree, info["observation_metadata"]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, default="https://www.amazon.com")
+    args = parser.parse_args()
+
+    clean_url = (
+        args.url.replace("https://", "")
+        .replace("http://", "")
+        .replace("file://", "")
+        .replace("/", "_")
+        .replace(".", "_")
+        .replace(":", "_")
+        .replace("?", "_")
+        .replace("=", "_")
+        .replace("&", "_")
+        .replace("%", "_")
+        .replace("#", "_")
+    )
+    path_out_no_ext = f"{path_of_this_file}/{clean_url}"
+    convert(args.url, path_out_no_ext)
---
+++ traj_cleaning/convert_webarena.py
@@ -0,0 +1,73 @@
+from convert_html import convert
+import argparse
+import os
+import glob
+from tqdm import tqdm
+from playwright._impl._api_types import TimeoutError
+
+
+def clean_and_convert_directory(directory, log_file='failed_files.log'):
+    trace_files = glob.glob(os.path.join(directory, '*.html'))
+    
+    for trace_file in trace_files:
+        try:
+            path_out_no_ext = os.path.splitext(trace_file)[0]
+            convert(f"file://{trace_file}", path_out_no_ext)
+        except TimeoutError:
+            print(f"Timeout occurred for file: {trace_file}")
+            log_failed_file(trace_file, log_file)
+            continue  # Skip to the next file
+
+def log_failed_file(file_path, log_file='failed_files.log'):
+    """
+    Log the file that caused a timeout error.
+
+    Args:
+    file_path (str): The path of the file that caused the timeout.
+    log_file (str): The path of the log file where failed files are recorded.
+    """
+    with open(log_file, 'a') as f:
+        f.write(file_path + '\n')
+
+
+def convert_file_path_to_url(file_path):
+    # Convert file path to a clean URL-like string for use in the conversion function
+    return (
+        file_path.replace("https://", "")
+        .replace("http://", "")
+        .replace("file://", "")
+        .replace("/", "_")
+        .replace(".", "_")
+        .replace(":", "_")
+        .replace("?", "_")
+        .replace("=", "_")
+        .replace("&", "_")
+        .replace("%", "_")
+        .replace("#", "_")
+    )
+
+def process_failed_files(log_file='failed_files.log'):
+    with open(log_file, 'r') as f:
+        failed_files = f.read().splitlines()
+    for trace_file in failed_files:
+        try:
+            clean_url = convert_file_path_to_url(trace_file)
+            path_out_no_ext = os.path.splitext(trace_file)[0]
+            convert(f"file://{trace_file}", path_out_no_ext)
+            print(f"Processed failed file: {trace_file}")
+        except TimeoutError:
+            print(f"Timeout occurred again for file: {trace_file}")
+            # You might choose to log this again or handle it differently
+            continue
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", type=str, required=True, help="Directory containing .trace.html files")
+    parser.add_argument("--process_failed", action='store_true', help="Process files from failed_files.log instead of the directory")
+    args = parser.parse_args()
+
+    if args.process_failed:
+        process_failed_files()
+    else:
+        print("start processing")
+        clean_and_convert_directory(args.directory)
